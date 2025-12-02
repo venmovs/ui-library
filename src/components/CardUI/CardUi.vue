912070ee1d74cec3e07da30f7b59aba83f6caa2b
Alex Lyubimov <lyubimovone@yandex.ru>
2025-12-02 22:48:05 +0300

    [ui_4] feature: CardUI

---
+++ src/components/CardUI/CardUi.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+ <div class="card">
+    <slot/>
+ </div>
+</template>
+
+
+<style scoped>
+
+.card {
+    display: inline-block;
+    border: 1px solid;
+    border-color: rgba(0, 0, 0, 0.05);
+    border-radius: 16px;
+    padding: 24px;
+    background-color: #fff;
+}
+
+</style>
