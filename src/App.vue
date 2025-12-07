9c06a146c1f2067181ed0041a45aa4a6ef4d9e1c
Alex Lyubimov <lyubimovone@yandex.ru>
2025-12-07 21:41:41 +0300

    [ui_6] feature: BreadcrumbUI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import ButtonUI from "./components/ButtonUI";
+import BreadcrumbUI from "./components/BreadcrumbUI/BreadcrumbUI.vue";
+
+const breadcrumbs = [ 
+  {name:'Breadcrumb_1',href:'/'},
+  {name:'Breadcrumb_2',href:'/'}, 
+  {name:'Breadcrumb_3'}
+]; 
+
 </script>
 
 <template>
   <ButtonUI label="Кнопка" rounded disabled />
   <input type="range" />
+  <BreadcrumbUI :breadcrumbs="breadcrumbs"/>
 </template>
